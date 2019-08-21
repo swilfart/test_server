84a2c89c613b6e36ca1f5aa216eed45ab8c31537
Ubuntu <ubuntu@ip-172-31-40-157.eu-west-3.compute.internal>
2019-08-21 07:56:55 +0000

    display 2

--- index.js
+++ index.js
@@ -52,5 +52,5 @@ app.use(function(err, req, res, next){
   }
 });
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
